68fc95f55796c1d07391f4e68eed51322c288e99
Rabin <rabing945h@gmail.com>
2023-07-20 16:38:35 +0545

    home page completed

--- play_it/lib/Screens/homePage.dart
+++ play_it/lib/Screens/homePage.dart
@@ -9,12 +9,15 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   // TextEditingController _searchController = TextEditingController();
+  int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-          appBar: AppBar(
+        child: Scaffold(
+       appBar: PreferredSize(
+          preferredSize: Size.fromHeight(50), // Set the desired height of the AppBar
+          child: AppBar(
             backgroundColor: Color(0xff00525d),
             actions: [
               Padding(
@@ -26,95 +29,399 @@ class _HomePageState extends State<HomePage> {
                 ),
               ),
             ],
+           
           ),
-          backgroundColor: Color(0xff00525d),
-          body: Container(
+        ),
+      backgroundColor: Color(0xff00525d),
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            scrollDirection: Axis.vertical,
             child: Column(
               children: [
-                //searchBox
-                Container(
-                  width: 200,
-                  height: 40,
-                  child: TextField(
-                    
-                    decoration: InputDecoration(
-
-                      hintText: 'Search....',
-                      hintStyle: TextStyle(fontStyle: FontStyle.italic),
-                      suffixIcon: Icon(Icons.search),
-                      filled: true,
-                      fillColor: Colors.white.withOpacity(1),
-                      contentPadding:EdgeInsets.only(top: 10.0,bottom: 5.0, left: 10.0) ,
-                      border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(100)))
-                    ),
+                // First Row
+                Padding(
+                  padding: const EdgeInsets.only(top:50),
+                  child: Column(
+                    children: [
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: Padding(
+                          padding: const EdgeInsets.only(
+                              left: 15), // Add left padding
+                          child: Container(
+                            child: const Text(
+                              'Start Listening..',
+                              style: TextStyle(
+                                fontStyle: FontStyle.normal,
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      SingleChildScrollView(
+                        scrollDirection: Axis
+                            .horizontal, // Set the scroll direction to horizontal
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            SizedBox(width: 15),
+                            Column(
+                              children: [
+                                Image(
+                                  image:
+                                      AssetImage('assets/images/eminiem.jpg'),
+                                  width: 150,
+                                  height: 150,
+                                  fit: BoxFit.cover,
+                                ),
+                                SizedBox(height: 5),
+                                Text(
+                                  'Hip Hop Music',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(width: 10),
+                            Column(
+                              children: [
+                                Image(
+                                  image: AssetImage(
+                                      'assets/images/edshrean.jpg'),
+                                  width: 150,
+                                  height: 150,
+                                  fit: BoxFit.cover,
+                                ),
+                                SizedBox(height: 5),
+                                Text(
+                                  'Pop Music',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(width: 10),
+                            Column(
+                              children: [
+                                Image(
+                                  image:
+                                      AssetImage('assets/images/metal.jpg'),
+                                  width: 150,
+                                  height: 150,
+                                  fit: BoxFit.cover,
+                                ),
+                                SizedBox(height: 5),
+                                Text(
+                                  'Rock Music',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(width: 10),
+                            Column(
+                              children: [
+                                Image(
+                                  image: AssetImage(
+                                      'assets/images/classical.jpg'),
+                                  width: 150,
+                                  height: 150,
+                                  fit: BoxFit.cover,
+                                ),
+                                SizedBox(height: 5),
+                                Text(
+                                  'Classical Music',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(width: 15),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                SizedBox(height: 15,),
-                Container(
-                  child:Column(children: [
-                    Align(
-                      alignment: Alignment.topLeft,
-                      child: Container(
-                      
-                        child: const Text('Start Listening......', style: TextStyle(
-                          fontStyle: FontStyle.normal,fontSize: 25,fontWeight: FontWeight.bold, color: Colors.white70,
-                        ),),
-                    
-                      ),
-                    )
+                SizedBox(height: 20),
 
-                  ]) ,
+                // Second Row For Recommendation
+                Container(
+                  child: Column(
+                    children: [
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 15),
+                          child: Container(
+                            child: const Text(
+                              'Recommended',
+                              style: TextStyle(
+                                fontStyle: FontStyle.normal,
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 15),
+                          child: Text(
+                            "Based on your Listening",
+                            style: TextStyle(
+                              fontStyle: FontStyle.normal,
+                              color: Colors.white70,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            SizedBox(width: 15),
+                            _buildRecommendation(
+                              image: 'assets/images/eminiem.jpg',
+                              title: 'Revival',
+                              subtitle: 'Eminem',
+                            ),
+                            SizedBox(width: 10),
+                            _buildRecommendation(
+                              image: 'assets/images/edshrean.jpg',
+                              title: 'Pop Music',
+                              subtitle: 'Ed Sheeran',
+                            ),
+                            SizedBox(width: 10),
+                            _buildRecommendation(
+                              image: 'assets/images/metal.jpg',
+                              title: 'Rock Music',
+                              subtitle: 'Metallica',
+                            ),
+                            SizedBox(width: 10),
+                            _buildRecommendation(
+                              image: 'assets/images/classical.jpg',
+                              title: 'Classical Music',
+                              subtitle: 'Ludwig van Beethoven',
+                            ),
+                            SizedBox(width: 15),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-                Container(),
+
+                SizedBox(height: 20),
+
+                // Third Row (You can replace this with your content)
+                Container(
+                  child: Column(
+                    children: [
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 15),
+                          child: Container(
+                            child: const Text(
+                              'Trending',
+                              style: TextStyle(
+                                fontStyle: FontStyle.normal,
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                     
+                      SizedBox(height: 10),
+                      SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            SizedBox(width: 15),
+                            _buildRecommendation(
+                              image: 'assets/images/eminiem.jpg',
+                              title: 'Revival',
+                              subtitle: 'Eminem',
+                            ),
+                            SizedBox(width: 10),
+                            _buildRecommendation(
+                              image: 'assets/images/edshrean.jpg',
+                              title: 'Pop Music',
+                              subtitle: 'Ed Sheeran',
+                            ),
+                            SizedBox(width: 10),
+                            _buildRecommendation(
+                              image: 'assets/images/metal.jpg',
+                              title: 'Rock Music',
+                              subtitle: 'Metallica',
+                            ),
+                            SizedBox(width: 10),
+                            _buildRecommendation(
+                              image: 'assets/images/classical.jpg',
+                              title: 'Classical Music',
+                              subtitle: 'Ludwig van Beethoven',
+                            ),
+                            SizedBox(width: 15),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
                 Container()
               ],
             ),
           ),
-          // body: Padding(
-          //   padding: const EdgeInsets.all(8.0),
-          //   child: Column(
-          //     children: [
 
-          //     Container(
-          //       width: 200,
-          //       height: 40,
-          //       decoration: BoxDecoration(
-          //         border: Border.all(),
-          //         color: Colors.white,
-          //         borderRadius: BorderRadius.circular(40.0),
+          // Fixed position search box at the top
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              height: 35, // Set the desired height for the search box
+              color: Color.fromARGB(255, 236, 236, 236).withOpacity(0),
+              child: Center(
+                child: Container(
+                  width: 250,
+                  height: 40,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Search...',
+                      hintStyle: TextStyle(fontStyle: FontStyle.italic),
+                      suffixIcon: Icon(Icons.search),
+                      filled: true,
+                      fillColor: Colors.white.withOpacity(1),
+                      contentPadding:
+                          EdgeInsets.only(top: 10.0, bottom: 5.0, left: 10.0),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(100)),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          // fixed music palyin poi=sition
+          // Positioned(
+          //   left: 0,
+          //   right:0,
+          //   bottom: 0,
+          //   child: 
+          // Container(
+          //   padding: EdgeInsets.symmetric(vertical: 10),
+          //  color: Color.fromARGB(255, 39, 38, 38).withOpacity(0.8),
+          //   child: Row(
+          //     children: [
+          //       Icon(
+          //         Icons.my_library_music_outlined,
+          //         size: 40,
+          //         color: Color.fromARGB(255, 216, 213, 213),
           //       ),
-          //       child: Stack(
-          //         alignment: Alignment.center,
-          //         children: [
-          //           Align(
-          //             alignment: Alignment.centerRight,
-          //             child: Padding(
-          //               padding: EdgeInsets.only(right: 8.0),
-          //               child: Icon(Icons.search),
-          //             ),
-          //           ),
-          //           TextField(
-          //             decoration: InputDecoration(
-          //               hintText: 'Search......',
-          //               border: OutlineInputBorder(
-          //                 borderRadius: BorderRadius.circular(40.0),
-          //               ),
-          //             ),
-          //           )
-          //         ],
-          //       ),
-          //     ),
-          //     SizedBox(
-          //       height: 16.0,
-          //     ),
-          //     Container(
-          //       child: Text("Start Listening...",
-          //           style: TextStyle(
-          //               color: Colors.white10.withOpacity(1),
-          //               fontSize: 18.0)),
-          //     )
-          //   ]),
-          // )),
+          //       Center(child: Padding(
+          //         padding: const EdgeInsets.only(left: 80),
+          //         child: Text("Now Playing", style: TextStyle( fontStyle: FontStyle.normal,
+          //                       fontSize: 20,
+          //                       fontWeight: FontWeight.bold,
+          //                       color: Colors.white70,), ),
+          //       )),
+          //       Padding(padding: const EdgeInsets.only(left: 90), 
+          //       child: Icon(Icons.play_arrow_rounded,size: 40, color: Colors.white70),
+          //       )
+          //     ],
+          //   ),
+
+          // ))
+         
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          backgroundColor: Color(0xff00525d),
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.white.withOpacity(0.6),
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.bookmarks_outlined),
+              label: 'Favorites',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: 'Settings',
+            ),
+          ]),
     ));
   }
+}
+
+Widget _buildRecommendation({
+  required String image,
+  required String title,
+  required String subtitle,
+}) {
+  return Column(
+    children: [
+      Image(
+        image: AssetImage(image),
+        width: 150,
+        height: 150,
+        fit: BoxFit.cover,
+      ),
+      SizedBox(height: 5),
+      Text(
+        title,
+        style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.w500,
+          color: Colors.white,
+        ),
+      ),
+      Text(
+        subtitle,
+        style: TextStyle(
+          fontSize: 16,
+          color: Colors.white70,
+        ),
+      ),
+    ],
+  );
 }
