18e142dc3b0e8013ad6d3cd9fbd8fd140ac8f7a8
Rabin <rabing945h@gmail.com>
2023-07-21 16:05:33 +0545

    signin commit

--- play_it/lib/Screens/Login/Signup.dart
+++ play_it/lib/Screens/Login/Signup.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:play_it/Screens/Login/Login.dart';
+import 'package:play_it/Screens/home.dart';
 
 class SignupScreen extends StatefulWidget {
   const SignupScreen({super.key});
@@ -8,10 +10,230 @@ class SignupScreen extends StatefulWidget {
 }
 
 class _SignupScreenState extends State<SignupScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Scaffold(appBar: AppBar(actions: [
-      
-    ]),));
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          leading: Container(),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.close),
+              onPressed: () {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => Home()));
+              },
+              color: Colors.white70,
+              iconSize: 30,
+            ),
+          ],
+          backgroundColor: Color(0xff00525d),
+        ),
+        backgroundColor: Color(0xff00525d),
+        body: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(5, 0, 110, 0),
+                child: Text(
+                  "CREATE ACCOUNT",
+                  style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(15, 0, 10, 0),
+                      child: TextFormField(
+                        controller: _nameController,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your name';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          prefixIcon: Icon(Icons.person_3_rounded),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            borderSide: BorderSide(color: Colors.blue),
+                          ),
+                          contentPadding: EdgeInsets.symmetric(
+                              vertical: 20.0,
+                              horizontal: 16.0), // Adjust the padding
+                          floatingLabelBehavior: FloatingLabelBehavior.never,
+                          errorStyle: TextStyle(
+                              color: Colors.red), // Set error text color
+                          hintText: 'Full Name',
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+                      child: TextFormField(
+                        controller: _emailController,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your Email';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(
+                            Icons.email_outlined,
+                          ),
+
+                          // Set error text color
+                          hintText: 'Email',
+                          hintStyle: TextStyle(color: Colors.white70),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+                      child: TextFormField(
+                        controller: _passwordController,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your Password';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(
+                            Icons.lock_clock_outlined,
+                          ),
+
+                          // Set error text color
+                          hintText: 'Password',
+                          hintStyle: TextStyle(color: Colors.white70),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+                      child: TextFormField(
+                        controller: _confirmPasswordController,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your Password';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(
+                            Icons.lock_clock_outlined,
+                          ),
+
+                          // Set error text color
+                          hintText: 'Confrim Password',
+                          hintStyle: TextStyle(color: Colors.white70),
+                        ),
+                      ),
+                    ),
+                     SizedBox(height: 100),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(230, 0, 10, 0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [
+                              Color.fromARGB(255, 2, 105, 119),
+                              Color.fromARGB(255, 0, 23, 26),
+                            ],
+                            begin: Alignment
+                                .centerLeft, // Alignment of the gradient starting point
+                            end: Alignment.centerRight,
+                          ),
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                        child: ElevatedButton.icon(
+                          onPressed: () {
+                            Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => LoginScreen()));
+
+                          },
+                          label: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Text(
+                                'SignUp',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              Icon(
+                                Icons.arrow_forward,
+                                color: Colors.white,
+                              ),
+                            ],
+                          ), 
+                          icon: SizedBox.shrink(),
+                          style: ElevatedButton.styleFrom(
+                            // To remove the default button background color and elevation
+                            primary: Colors.transparent,
+                            elevation: 0,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white
+                  ),
+                  child: Text("Google"),
+                ),
+                
+              ),
+              Container(),
+             
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
+
+
+
+
+
+
+
--- play_it/lib/Screens/home.dart
+++ play_it/lib/Screens/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:play_it/Screens/Login/Signup.dart';
 import 'package:play_it/Screens/homePage.dart';
+// import 'package:play_it/Screens/playtoon/analytics.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -93,7 +95,7 @@ class _HomeState extends State<Home> {
                         ),
                         ElevatedButton(
                           onPressed: () {
-                            Navigator.push(context, MaterialPageRoute(builder:(context) => HomePage()));
+                            Navigator.push(context, MaterialPageRoute(builder:(context) => SignupScreen()));
                           },
                           style: ButtonStyle(
                             backgroundColor:
